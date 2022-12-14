19bfd463b5e877fcbe57e129c98aea22f5fe9871
Matt Rohatynskyj <mjr593@york.ac.uk>
2022-12-14 16:07:59 +0000

    Added Screen Classes

---
+++ core/src/com/mygdx/game/MainMenuScreen.java
@@ -0,0 +1,72 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class MainMenuScreen implements Screen {
+
+    final PiazzaPanic game;
+    OrthographicCamera camera;
+
+    public MainMenuScreen(final PiazzaPanic game) {
+        this.game = game;
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 800, 480);
+    }
+
+    @Override
+    public void render(float delta) {
+        ScreenUtils.clear(0, 0.2f, 0, 1);
+
+        camera.update();
+        game.batch.setProjectionMatrix(camera.combined);
+
+        game.batch.begin();
+        game.font.draw(game.batch, "PiazzaPanic", 100, 150);
+        game.font.draw(game.batch, "Click to start!", 100, 100);
+        game.batch.end();
+
+        if (Gdx.input.isTouched()) {
+            game.setScreen(new GameScreen(game));
+            dispose();
+        }
+
+    }
+
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void show() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void hide() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void dispose() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void resume() {
+        // TODO Auto-generated method stub
+
+    }
+}
