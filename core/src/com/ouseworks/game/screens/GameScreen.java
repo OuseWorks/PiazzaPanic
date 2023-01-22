df2cdc7a55f9e3840adbde0ae1986efdf5049734
= <benharris@manx.net>
2023-01-22 21:52:46 +0000

    Added a top HUD to keep track of time, reputation points and customers served :)

--- core/src/com/ouseworks/game/screens/GameScreen.java
+++ core/src/com/ouseworks/game/screens/GameScreen.java
@@ -1,6 +1,5 @@
 package com.ouseworks.game.screens;
 
-import com.badlogic.ashley.core.Entity;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.maps.tiled.TiledMap;
@@ -9,7 +8,9 @@ import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.Vector3;
 
+import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.ouseworks.game.EntityFactory;
+import com.ouseworks.game.TopHud;
 import com.ouseworks.game.systems.RenderEntitySystem;
 import com.ouseworks.game.PiazzaPanicGame;
 
@@ -18,6 +19,10 @@ public class GameScreen implements Screen {
     private TiledMap map;
     private OrthogonalTiledMapRenderer renderer;
     private OrthographicCamera camera;
+    private TopHud hud;
+    private Stage stage;
+
+
 
     public GameScreen(final PiazzaPanicGame game) {
         this.game = game;
@@ -25,14 +30,15 @@ public class GameScreen implements Screen {
         entityFactory.createCook(300,400,"Chef1.png");
         entityFactory.createCook(200,500,"Chef2.png");
         entityFactory.createCustomer(600,600,game.engine.getEntities().get(0),"Item.png");
-
     }
 
     public void render(float delta) {
+        hud.update(delta);
         renderer.setView(camera);
         renderer.render();
-
         game.engine.update(delta);
+        game.batch.setProjectionMatrix(hud.stage.getCamera().combined); //set the spriteBatch to draw what our stageViewport sees
+        hud.stage.draw();
     }
 
     @Override
@@ -44,7 +50,6 @@ public class GameScreen implements Screen {
                 layer0.getHeight() * layer0.getTileHeight() / 2, 0);
         camera.position.set(center);
         camera.update();
-
     }
 
     @Override
@@ -53,6 +58,8 @@ public class GameScreen implements Screen {
         renderer = new OrthogonalTiledMapRenderer(map);
         camera = new OrthographicCamera();
         game.engine.addSystem(new RenderEntitySystem(camera,game.batch));
+        hud = new TopHud(game.batch,500);
+
     }
 
     @Override
