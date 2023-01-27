dc61b6a6c180ba2f601f9dfaac8d5bd0d16c9e18
Matt Rohatynskyj <mjr593@york.ac.uk>
2023-01-27 08:22:37 +0000

    fixed top wall in tilemap

--- core/src/com/ouseworks/game/EntityFactory.java
+++ core/src/com/ouseworks/game/EntityFactory.java
@@ -7,19 +7,21 @@ import com.ouseworks.game.components.*;
 public class EntityFactory {
 
     public Engine engine;
-    public EntityFactory(Engine engine){
-        this.engine=engine;
+
+    public EntityFactory(Engine engine) {
+        this.engine = engine;
     }
 
-    // TODO Explore possibility of creating config files to store the description of entities.
-    public void createCook(int x, int y, String img, boolean movable){
+    // TODO Explore possibility of creating config files to store the description of
+    // entities.
+    public void createCook(int x, int y, String img, boolean movable) {
         final Entity cook = engine.createEntity();
         cook.add(engine.createComponent(PositionComponent.class));
         cook.add(engine.createComponent(RenderComponent.class));
-        if(movable){
+        if (movable) {
             cook.add(engine.createComponent(MoveableComponent.class));
             // Set cook movement speed
-            //cook.getComponent(MoveableComponent.class).speed = ;
+            // cook.getComponent(MoveableComponent.class).speed = ;
         }
         cook.add(engine.createComponent(ClickableComponent.class));
         cook.add(engine.createComponent(InventoryComponent.class));
@@ -27,18 +29,16 @@ public class EntityFactory {
         cook.add(engine.createComponent(CollideableComponent.class));
 
         // Set cook spawn location.
-        cook.getComponent(PositionComponent.class).x=x;
-        cook.getComponent(PositionComponent.class).y=y;
+        cook.getComponent(PositionComponent.class).x = x;
+        cook.getComponent(PositionComponent.class).y = y;
 
         // Set cook sprite image
-        cook.getComponent(RenderComponent.class).visual=img;
-
-
+        cook.getComponent(RenderComponent.class).visual = img;
 
         engine.addEntity(cook);
     }
 
-    public void createCustomer(int x, int y, Entity orderPlaced, String img){
+    public void createCustomer(int x, int y, Entity orderPlaced, String img) {
         final Entity customer = engine.createEntity();
         customer.add(engine.createComponent(PositionComponent.class));
         customer.add(engine.createComponent(RenderComponent.class));
@@ -46,12 +46,12 @@ public class EntityFactory {
         customer.add(engine.createComponent(BusyComponent.class));
         customer.add(engine.createComponent(CollideableComponent.class));
         // Set customer spawn location.
-        customer.getComponent(PositionComponent.class).x=100;
-        customer.getComponent(PositionComponent.class).y=100;
+        customer.getComponent(PositionComponent.class).x = 100;
+        customer.getComponent(PositionComponent.class).y = 100;
         // Set customer sprite image
-        customer.getComponent(RenderComponent.class).visual=img;
+        customer.getComponent(RenderComponent.class).visual = img;
         // Set customer order
-        customer.getComponent(OrderComponent.class).orderPlaced=orderPlaced;
+        customer.getComponent(OrderComponent.class).orderPlaced = orderPlaced;
         engine.addEntity(customer);
     }
 
