77bb00eb12cf5650b93ef60ed987998d849e9f2b
Matt Rohatynskyj <mjr593@york.ac.uk>
2023-01-29 14:23:50 +0000

    updated tilemap

---
+++ assets/RedBrickWall.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="RedBrickWall" tilewidth="64" tileheight="64" tilecount="9" columns="3">
+ <image source="redBrick.png" width="192" height="192"/>
+</tileset>
