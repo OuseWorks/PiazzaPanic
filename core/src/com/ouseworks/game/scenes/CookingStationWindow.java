a84ed6ec53a8932d0e89980271aeb96e7dd6725d
Matt Rohatynskyj <mjr593@york.ac.uk>
2023-01-31 10:59:32 +0000

    added cook burger button

--- core/src/com/ouseworks/game/scenes/CookingStationWindow.java
+++ core/src/com/ouseworks/game/scenes/CookingStationWindow.java
@@ -2,17 +2,25 @@ package com.ouseworks.game.scenes;
 
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Window;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class CookingStationWindow {
     public Stage stage;
     public Window window;
+    public ImageButton cookBurger;
+    public Label cookBurgerLabel;
 
     public CookingStationWindow(Stage stage) {
         this.stage = stage;
@@ -23,12 +31,18 @@ public class CookingStationWindow {
         window.setPosition(Gdx.graphics.getWidth() / 2 - 300, Gdx.graphics.getHeight() / 2 - 225);
 
         Table table = new Table();
-        table.top();
         table.setFillParent(true);
-        table.setDebug(true);
+
+        Texture burgerTexture = new Texture(Gdx.files.internal("burger64.png"));
+        Drawable burgerImage = new TextureRegionDrawable(new TextureRegion(burgerTexture));
+
+        cookBurger = new ImageButton(burgerImage);
+        cookBurgerLabel = new Label("Cook Burger", skin);
+        table.add(cookBurgerLabel);
+        table.row();
+        table.add(cookBurger);
 
         TextButton closeButton = new TextButton("Close", skin);
-        closeButton.bottom().right();
         closeButton.setColor(Color.RED);
         closeButton.addListener(new ChangeListener() {
             public void changed(ChangeEvent event, Actor actor) {
@@ -37,6 +51,8 @@ public class CookingStationWindow {
         });
 
         table.addActor(closeButton);
+        closeButton.top().right();
+
         window.addActor(table);
         stage.addActor(window);
     }
